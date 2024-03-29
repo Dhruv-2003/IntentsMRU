9c09a3263457b01f014d5469710d4bac9738c989
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-03-29 20:03:22 +0530

    feat: created state for SolverIntent system

--- intentsSystem/src/state.ts
+++ intentsSystem/src/state.ts
@@ -1,57 +1,101 @@
 import { State } from "@stackr/sdk/machine";
-import { BytesLike, ZeroHash, solidityPackedKeccak256 } from "ethers";
+import {
+  AddressLike,
+  BytesLike,
+  ZeroHash,
+  solidityPackedKeccak256,
+} from "ethers";
 import { MerkleTree } from "merkletreejs";
 
-export type Leaves = {
-  address: string;
-  balance: number;
-  nonce: number;
-  allowances: {
-    address: string;
-    amount: number;
-  }[];
-}[];
+export type IntentType = {
+  requestId: number;
+  userAddress: AddressLike;
+  solverAddress: AddressLike;
+  intent: string;
+  params: any[]; // in format [param1 , param2]
+  ABI: string; // in format "function name(uint param1, bytes param2)"
+  protocolAddress: AddressLike; // to address
+  txValue: number; // in ethers format
+  solvedTxData: {}; // { to: ,  data: , value:  }
+};
 
-export class BetterMerkleTree {
+export type SolverMarketState = {
+  intents: IntentType[];
+  totalRequests: number;
+  solvers: AddressLike[];
+};
+
+export class SolverMarketTransport {
   public merkleTree: MerkleTree;
-  public leaves: Leaves;
+  public intents: IntentType[];
+  public totalRequests: number;
+  public solvers: AddressLike[];
 
-  constructor(leaves: Leaves) {
-    this.merkleTree = this.createTree(leaves);
-    this.leaves = leaves;
+  constructor(state: SolverMarketState) {
+    this.merkleTree = this.createTree(state.intents);
+    this.intents = state.intents;
+    this.totalRequests = state.totalRequests;
+    this.solvers = state.solvers;
   }
 
-  createTree(leaves: Leaves) {
+  createTree(leaves: IntentType[]) {
     const hashedLeaves = leaves.map((leaf) => {
       return solidityPackedKeccak256(
-        ["address", "uint256", "uint256"],
-        [leaf.address, leaf.balance, leaf.nonce]
+        [
+          "uint256",
+          "address",
+          "address",
+          "string",
+          "string",
+          "string",
+          "address",
+          "uint",
+          "string",
+        ],
+        [
+          leaf.requestId,
+          leaf.userAddress,
+          leaf.solverAddress,
+          leaf.intent,
+          JSON.stringify(leaf.params),
+          leaf.ABI,
+          leaf.protocolAddress,
+          leaf.txValue,
+          JSON.stringify(leaf.solvedTxData),
+        ]
       );
     });
     return new MerkleTree(hashedLeaves);
   }
 }
 
-export class ERC20 extends State<Leaves, BetterMerkleTree> {
-  constructor(state: Leaves) {
+export class SolverMarket extends State<
+  SolverMarketState,
+  SolverMarketTransport
+> {
+  constructor(state: SolverMarketState) {
     super(state);
   }
 
-  wrap(state: Leaves): BetterMerkleTree {
-    const newTree = new BetterMerkleTree(state);
+  wrap(state: SolverMarketState): SolverMarketTransport {
+    const newTree = new SolverMarketTransport(state);
     return newTree;
   }
 
-  clone(): State<Leaves, BetterMerkleTree> {
-    return new ERC20(this.unwrap());
+  clone(): State<SolverMarketState, SolverMarketTransport> {
+    return new SolverMarket(this.unwrap());
   }
 
-  unwrap(): Leaves {
-    return this.wrappedState.leaves;
+  unwrap(): SolverMarketState {
+    return {
+      intents: this.wrappedState.intents,
+      totalRequests: this.wrappedState.totalRequests,
+      solvers: this.wrappedState.solvers,
+    };
   }
 
   calculateRoot(): BytesLike {
-    if (this.wrappedState.leaves.length === 0) {
+    if (this.wrappedState.intents.length === 0) {
       return ZeroHash;
     }
     return this.wrappedState.merkleTree.getHexRoot();
